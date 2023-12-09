d4164b8d49cef6afe13a16b3690d6029519d4a5b
BrittLGiles <145483765+BrittLGiles@users.noreply.github.com>
2023-12-09 14:49:08 -0700

    project

---
+++ scripts/randomcat.js
@@ -0,0 +1,16 @@
+document.getElementById('refresh-button').addEventListener('click', function() {
+   fetchCatImage();
+});
+
+function fetchCatImage() {
+   fetch('https://api.thecatapi.com/v1/images/search')
+       .then(response => response.json())
+       .then(data => {
+           document.getElementById('cat-image').src = data[0].url;
+       })
+       .catch(error => {
+           console.error('Error fetching cat image:', error);
+       });
+}
+
+fetchCatImage();
